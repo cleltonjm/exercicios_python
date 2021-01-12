3b85536d5b629af28279596410b91f7561dbee41
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2021-01-12 17:08:47 -0300

    Add files via upload

--- desafio03.py
+++ desafio03.py
@@ -1,9 +1,6 @@
-print('====== DESAFIO 02 ======')
+print('====== DESAFIO 03 ======')
 
-n1 = input('Primeiro número = ')
-n2 = input('Segundo número = ')
+n1 = int(input('Digite um número = '))
+n2 = int(input('Digite outro número = '))
 
-Num1 = int(n1)
-Num2 = int(n2)
-
-print('A soma é ', Num1 + Num2)
+print('A soma é ', n1 + n2)

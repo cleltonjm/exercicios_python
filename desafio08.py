2f603af843fec4a64590365a448311e56f6c29cb
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2021-01-13 17:15:57 -0300

    Add files via upload

---
+++ desafio08.py
@@ -0,0 +1,2 @@
+metro = float(input('Digite um valor em metros: '))
+print('Esse valor convertido em centímetros é {}cm e em milímetro é {}mm'.format(metro*100, metro*1000))
---
+++ desafio10.py
@@ -0,0 +1,2 @@
+n1 = float(input('Quantos dinheiro você tem na carteira? R$'))
+print('Com R${:.2f} você pode comprar US${:.2f} '.format(n1, n1/3.27))

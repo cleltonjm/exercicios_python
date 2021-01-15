794c53db5c04eeb95a0e2210970892161291d51b
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2021-01-15 14:16:51 -0300

    Add files via upload

---
+++ desafio16.py
@@ -0,0 +1,3 @@
+from math import trunc
+num = float(input('Digite um número real: '))
+print('Você digitou {}, a porção inteira dele é {}'.format(num, trunc(num)))
---
+++ desafio21.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('desafio21.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+while(pygame.mixer.music.get_busy()): pass
+
